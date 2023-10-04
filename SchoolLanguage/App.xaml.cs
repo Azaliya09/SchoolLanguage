78b31a916cdb0972b1ff0431b99bb9e55232a0e8
Azaliya09 <azaliyasirachova4@gmail.com>
2023-10-04 08:32:43 +0300

    added connect database

--- SchoolLanguage/App.xaml.cs
+++ SchoolLanguage/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SchoolLanguage.Base;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,6 @@ namespace SchoolLanguage
     /// </summary>
     public partial class App : Application
     {
+        public static SchoolLanguageAzaliya09Entities db = new SchoolLanguageAzaliya09Entities();
     }
 }
