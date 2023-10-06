e9bd8fb6ca55d6fd9945c1a64debe75b11674f21
Azaliya09 <azaliyasirachova4@gmail.com>
2023-10-06 16:38:50 +0300

    added ServicesList

---
+++ SchoolLanguage/Components/ServiceUserControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using SchoolLanguage.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SchoolLanguage.Components
+{
+    /// <summary>
+    /// Логика взаимодействия для ServiceUserControl.xaml
+    /// </summary>
+    public partial class ServiceUserControl : UserControl
+    {
+        public ServiceUserControl(Service service)
+        {
+            InitializeComponent();
+            TitleTb.Text = service.Title;
+            CostTimeTb.Text = service.costTimeStr;
+            DiscountTb.Text = service.DiscountStr;
+            CostTb.Text = (service.Cost).ToString("N0") + " ";
+            CostTb.Visibility = service.CostVisibility;
+
+        }
+    }
+}
--- SchoolLanguage/MainWindow.xaml.cs
+++ SchoolLanguage/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SchoolLanguage.Pages;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,14 @@ namespace SchoolLanguage
         public MainWindow()
         {
             InitializeComponent();
+            //var path = @"C:\Users\212118\Desktop\Task\Сессия 1\services_s_import\";
+            //foreach(var item in App.db.Service.ToArray())
+            //{
+            //    var fullPath = path + item.MainImagePath;
+            //    item.MainImage = File.ReadAllBytes(fullPath);
+            //}
+            //App.db.SaveChanges();
+            MainFrame.Navigate(new ServiceListPage());
         }
     }
 }
