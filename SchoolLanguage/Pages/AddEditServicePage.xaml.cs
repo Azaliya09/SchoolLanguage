7007039369e4907c82a83e59b52204469132f5ba
Azaliya09 <azaliyasirachova4@gmail.com>
2023-11-01 10:20:09 +0300

    added listview with image

--- SchoolLanguage/Pages/AddEditServicePage.xaml.cs
+++ SchoolLanguage/Pages/AddEditServicePage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using SchoolLanguage.Base;
+﻿using Microsoft.Win32;
+using SchoolLanguage.Base;
+using SchoolLanguage.Components;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,6 +30,78 @@ namespace SchoolLanguage.Pages
             InitializeComponent();
             service = _service;
             this.DataContext = service;
+            PhotoList.ItemsSource = App.db.ServicePhoto.Where(x => x.ServiceID == service.ID).ToList();
+        }
+
+        private void AddImageBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog()
+            {
+                Filter = "*.png|*.png|*.jpeg|*.jpeg|*.jpg|*.jpg"
+            };
+            
+            if (openFile.ShowDialog().GetValueOrDefault())
+            {
+                App.db.ServicePhoto.Add(new ServicePhoto()
+                {
+                    ServiceID = service.ID,
+                    PhotoPath = File.ReadAllBytes(openFile.FileName)
+                });
+                App.db.SaveChanges();
+                PhotoList.ItemsSource = App.db.ServicePhoto.Where(x => x.ServiceID == service.ID).ToList();
+            }
+        }
+
+        private void DeleteImageBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var selectPhoto = PhotoList.SelectedItem as ServicePhoto;
+            if(selectPhoto != null)
+            {
+                App.db.ServicePhoto.Remove(selectPhoto);
+            }
+            else
+            {
+                MessageBox.Show("Ничего не выбрано!");
+            }
+            App.db.SaveChanges();
+            PhotoList.ItemsSource = App.db.ServicePhoto.Where(x => x.ServiceID == service.ID).ToList();
+        }
+
+        private void EditImageBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog()
+            {
+                Filter = "*.png|*.png|*.jpeg|*.jpeg|*.jpg|*.jpg"
+            };
+            openFile.ShowDialog();
+            if (openFile.FileName != null)
+            {
+                service.MainImage = File.ReadAllBytes(openFile.FileName);
+                ImageImg.Source = new BitmapImage(new Uri(openFile.FileName));
+            }
+
+        }
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder error = new StringBuilder();
+            if (service.DurationInSeconds > 14400)
+                error.AppendLine("Услуга не может превышать 4 часа! ");
+
+            if (service.ID == 0)
+            {
+                Service newService = App.db.Service.Add(service);
+                if (App.db.Service.Any(x => x.Title == service.Title))
+                    error.AppendLine("Услуга с таким именем уже существует! ");
+
+            }
+            if (error.Length > 0)
+            {
+                MessageBox.Show(error.ToString());
+                return;
+            }
+            App.db.SaveChanges();
+            Navigation.NextPage(new PageComponent("Список услуг", new ServiceListPage()));
+
         }
     }
 }
